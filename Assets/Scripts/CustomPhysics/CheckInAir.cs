b7e72307119306f4ac8863e636cbc5f3cc92c892
Ryan Andersen <randersen3@gatech.edu>
2016-08-22 12:49:25 -0400

    CheckInAir now added
    -Jump mechanics now use check in air script before allowing the player
    to jump
    -Some inputs have been added for keyboard use

---
+++ Assets/Scripts/CustomPhysics/CheckInAir.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckInAir : MonoBehaviour {
+    public bool inAir = false;
+    public bool lockInAir = false;
+    public float checkDistance = .01f;
+    public Transform[] checkPositions = new Transform[3];
+
+    void FixedUpdate()
+    {
+        if (lockInAir) return;
+        checkInAir();
+    }
+
+    void checkInAir()
+    {
+        inAir = true;
+        foreach(Transform t in checkPositions)
+        {
+            Ray2D r = new Ray2D(t.position, Vector2.down);
+            if (Physics2D.Raycast(r.origin, r.direction, checkDistance))
+            {
+                inAir = false;
+                return;
+            }
+
+        }
+    }
+}
