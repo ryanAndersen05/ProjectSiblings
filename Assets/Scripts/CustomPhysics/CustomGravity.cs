df82ddf39051f6524d1548fce6ceaa7a57d06b35
Ryan Andersen <randersen3@gatech.edu>
2016-08-22 16:16:24 -0400

    Gravity now uses checkInAir script

--- Assets/Scripts/CustomPhysics/CustomGravity.cs
+++ Assets/Scripts/CustomPhysics/CustomGravity.cs
@@ -9,15 +9,18 @@ public class CustomGravity : MonoBehaviour {
 
     public const float gravityForce = 9.8f;
     Rigidbody2D rigid;
+    CheckInAir checkInAir;
 
     void Start()
     {
         this.rigid = GetComponent<Rigidbody2D>();
+        checkInAir = GetComponentInChildren<CheckInAir>();
     }
 
 
     void FixedUpdate()
     {
+        if (!checkInAir.inAir) return;
         float y = rigid.velocity.y;
         y = Mathf.MoveTowards(y, -maxFallingSpeed, Time.fixedDeltaTime * gravityForce * gravityScale);
         rigid.velocity = new Vector2(rigid.velocity.x, y);
