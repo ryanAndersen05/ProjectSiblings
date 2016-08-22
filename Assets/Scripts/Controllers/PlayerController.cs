f14a508e509c50a243406e6e864b72ddab6eb0fc
Ryan Andersen <randersen3@gatech.edu>
2016-08-22 11:30:21 -0400

    Basic Jump Mechanics added
    -Needs an in air checker so that she does not jump infinitely

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -3,18 +3,21 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
     MovementMechanics mMechanics;
+    JumpMechanics jMechanics;
     Dialogue dialogue;
 
     void Start()
     {
         mMechanics = GetComponent<MovementMechanics>();
         dialogue = GetComponent<Dialogue>();
+        jMechanics = GetComponent<JumpMechanics>();
     }
 
 	void Update()
     {
         float hInput = Input.GetAxisRaw("Horizontal");
         bool action = Input.GetButtonDown("Action");
+        bool jump = Input.GetButtonDown("Jump");
         if (mMechanics != null)
         {
             mMechanics.setHorizontalInput(hInput);
@@ -22,6 +25,10 @@ public class PlayerController : MonoBehaviour {
         if (dialogue != null)
         {
             dialogue.activatedDialogue(action);
+        }
+        if (jMechanics != null)
+        {
+            jMechanics.activateJump(jump);
         }
     }
 }
--- Assets/Scripts/GameMechanics/JumpMechanics.cs
+++ Assets/Scripts/GameMechanics/JumpMechanics.cs
@@ -3,9 +3,18 @@ using System.Collections;
 
 public class JumpMechanics : MonoBehaviour {
     public float jumpForce = 15;
+    Rigidbody2D rigid;
 
-    public void activateJump()
+    void Start()
     {
+        rigid = GetComponent<Rigidbody2D>();
+    }
 
+    public void activateJump(bool jumpInput)
+    {
+        if (jumpInput)
+        {
+            rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
