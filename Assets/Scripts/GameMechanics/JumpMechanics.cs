6ea5de024f6bbde720e724218cae9d2eb4024430
Ryan Andersen <randersen3@gatech.edu>
2016-08-22 12:50:29 -0400

    jumpmechanics...

--- Assets/Scripts/GameMechanics/JumpMechanics.cs
+++ Assets/Scripts/GameMechanics/JumpMechanics.cs
@@ -3,16 +3,18 @@ using System.Collections;
 
 public class JumpMechanics : MonoBehaviour {
     public float jumpForce = 15;
+    CheckInAir checkInAir;
     Rigidbody2D rigid;
 
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        checkInAir = GetComponentInChildren<CheckInAir>();
     }
 
     public void activateJump(bool jumpInput)
     {
-        if (jumpInput)
+        if (jumpInput && !checkInAir.inAir)
         {
             rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
