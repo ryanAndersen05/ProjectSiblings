9b52880a7fb7f789c53a6c5e9180374576e68cab
Ryan Andersen <randersen3@gatech.edu>
2016-08-29 11:35:11 -0400

    Talk action icon added
    -Basic icon added to indicate when player is in range to communicate
    with NPC

--- Assets/Scripts/GameMechanics/Dialogue/NPCDialogue.cs
+++ Assets/Scripts/GameMechanics/Dialogue/NPCDialogue.cs
@@ -1,6 +1,27 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class NPCDialogue : MonoBehaviour {
     public string dialogueFileName;
+    public Transform actionIcon;
+
+    List<Collider2D> allColliders = new List<Collider2D>();
+
+    void OnTriggerEnter2D (Collider2D collider)
+    {
+        allColliders.Add(collider);
+        actionIcon.gameObject.SetActive(true);
+    }
+
+    void OnTriggerExit2D (Collider2D collider)
+    {
+        if (allColliders.Contains(collider))
+        {
+            allColliders.Remove(collider);
+        }
+        if (allColliders.Count <= 0)
+        {
+            actionIcon.gameObject.SetActive(false);
+        } 
+    }
 }
