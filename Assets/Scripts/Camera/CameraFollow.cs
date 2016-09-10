6d4e45e74450749c12cb2f2b0aeef6902a3b8589
Ryan Andersen <randersen3@gatech.edu>
2016-09-09 22:16:26 -0400

    Added a script to set maximum distance away from player

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -2,9 +2,12 @@
 using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
+    public float acceleration = 5;
+    public float maxXOffset = float.PositiveInfinity;
+    public float maxYOffset = float.PositiveInfinity;
     private float xOffset;
     private float yOffset;
-    public float acceleration = 5;
+    
 
     Transform currentTarget;
 
@@ -23,5 +26,9 @@ public class CameraFollow : MonoBehaviour {
         Vector3 goalPosition = new Vector3(currentTarget.position.x + xOffset,
             currentTarget.position.y + yOffset, transform.position.z);
         transform.position = Vector3.LerpUnclamped(transform.position, goalPosition, Time.deltaTime * acceleration);
+        if (Mathf.Abs(transform.position.y - currentTarget.position.y) > maxYOffset)
+        {
+            transform.position = new Vector3(transform.position.x, currentTarget.position.y + maxYOffset, transform.position.z);
+        }
     }
 }
