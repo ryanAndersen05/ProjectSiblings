e07b65d380ff09ba3cd0b1d54fddec185339c381
Ryan Andersen <randersen3@gatech.edu>
2016-09-17 19:36:58 -0400

    Money mechanics started

---
+++ Assets/Scripts/GameMechanics/Items/MoneyManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoneyManager : MonoBehaviour {
+    public int maxMoney = 10000;
+    public int currentMoney = 150;
+
+    public void loseMoney(int moneyLost)
+    {
+        currentMoney -= moneyLost;
+        if (currentMoney < 0)
+        {
+            currentMoney = 0;
+        }
+    }
+
+    public bool checkHasEnoughMoney(int checkAmount)
+    {
+        return currentMoney - checkAmount >= 0;
+    }
+
+    public bool addMoney(int moneyAdd)
+    {
+        if (currentMoney >= maxMoney) return false;
+        currentMoney += moneyAdd;
+        if (currentMoney > maxMoney) currentMoney = maxMoney;
+        return true;
+    }
+}
