f66a998f6132e8deaa00713700fec730ebc1f3ee
Ryan Andersen <randersen3@gatech.edu>
2016-09-17 22:37:06 -0400

    Money pick ups added to game

---
+++ Assets/Scripts/GameMechanics/Items/Item/Money.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Money : MonoBehaviour {
+    public int value = 1;
+
+
+    void OnTriggerEnter2D (Collider2D collider)
+    {
+        MoneyManager mManager = collider.GetComponent<MoneyManager>();
+        if (mManager != null)
+        {
+            mManager.addMoney(value);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MoneyUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MoneyUI : MonoBehaviour {
+    public Text moneyText;
+    MoneyManager mManager;
+
+    void Start()
+    {
+        mManager = GameObject.FindObjectOfType<MoneyManager>();
+    }
+
+    void Update()
+    {
+        moneyText.text = "x" + mManager.currentMoney;
+    }
+}
