42847b1f8ceff512df7a0479d711228a0ab1a41d
Ryan Andersen <randersen3@gatech.edu>
2016-10-17 23:12:55 -0400

    collider updates

---
+++ Assets/Scripts/ColliderOptimization.cs
@@ -0,0 +1,183 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class ColliderOptimization : MonoBehaviour {
+    void Start()
+    {
+        List<PolyShape> polyShapes = getPolygonShapes();
+        constructPolygonColliders(polyShapes);
+        Destroy(this);
+    }
+
+    bool optimizePolygons(List<PolyShape> polygonShapes)
+    {
+        for (int i = 0; i < polygonShapes.Count - 1; i++)
+        {
+            for (int j = i + 1; j < polygonShapes.Count; j++)
+            {
+                PolyShape p1 = polygonShapes[i];
+                PolyShape p2 = polygonShapes[j];
+            }
+        }
+        return false;
+    }
+
+    void constructPolygonColliders(List<PolyShape> polyShapes)
+    {
+        foreach (PolyShape p in polyShapes)
+        {
+
+            PolygonCollider2D pCollider = this.gameObject.AddComponent<PolygonCollider2D>();
+            pCollider.points = p.getVectorPoints();
+        }
+    }
+
+    List<PolyShape> getPolygonShapes()
+    {
+        List<PolyShape> polygonShapes = new List<PolyShape>();
+        BoxCollider2D[] boxColliders = GetComponentsInChildren<BoxCollider2D>();
+        foreach (BoxCollider2D b in boxColliders)
+        {
+            polygonShapes.Add(new PolyShape(getBoxColliderPoints(b)));
+            Destroy(b);
+        }
+        return polygonShapes;
+    }
+
+    Vector2[] getBoxColliderPoints(BoxCollider2D b)
+    {
+        Vector3 min = b.bounds.min;
+        Vector3 max = b.bounds.max;
+        Vector2[] vPoints = new Vector2[4];
+        float minX = min.x - transform.position.x;
+        float minY = min.y - transform.position.y;
+        float maxX = max.x - transform.position.x;
+        float maxY = max.y - transform.position.y;
+        vPoints[0] = new Vector2(minX, maxY);
+        vPoints[1] = new Vector2(maxX, maxY);
+        vPoints[2] = new Vector2(maxX, minY);
+        vPoints[3] = new Vector2(minX, minY);
+        foreach (Vector2 v in vPoints)
+        {
+            print(v);
+        }
+        return vPoints;
+    }
+
+    private class PolyShape
+    {
+        public List<PolyPoint> polyPoints = new List<PolyPoint>();
+        public List<PolyVector> polyVectors = new List<PolyVector>();
+
+        public PolyShape(Vector2[] points)
+        {
+
+            foreach (Vector2 v in points)
+            {
+                polyPoints.Add(new PolyPoint(v.x, v.y));
+            }
+
+            for (int i = 0; i < polyPoints.Count; i++)
+            {
+                polyVectors.Add(new PolyVector(polyPoints[i], polyPoints[(i + 1) % polyPoints.Count]));
+            }
+        }
+
+        public Vector2[] getVectorPoints()
+        {
+            Vector2[] vecPoints = new Vector2[polyPoints.Count];
+            for (int i = 0; i < polyPoints.Count; i++)
+            {
+                vecPoints[i] = new Vector2(polyPoints[i].x, polyPoints[i].y);
+            }
+            return vecPoints;
+        }
+
+        public PolyVector comparePolyShape(PolyShape p)
+        {
+            
+            foreach (PolyVector v1 in p.polyVectors)
+            {
+                foreach (PolyVector v2 in this.polyVectors)
+                {
+                    if (v2.compareVector(v1))
+                    {
+                        return v1;
+                    }
+                }
+            }
+            return null;
+        }
+
+        PolyShape combineShape2Point(PolyShape p, PolyVector v)
+        {
+            List<Vector2> newPoints = new List<Vector2>();
+            for (int i = 0; i < this.polyPoints.Count; i++)
+            {
+                
+            }
+            return new PolyShape(newPoints.ToArray());
+        }
+
+        public static PolyShape optimizePolyShape(PolyShape p1, PolyShape p2)
+        {
+            PolyVector v = p1.comparePolyShape(p2);
+            if (v != null)
+            {
+                List<Vector2> newPoints = new List<Vector2>();
+
+                
+            }
+            return null;
+        }
+    }
+
+    private class PolyPoint
+    {
+        public float x;
+        public float y;
+
+        public PolyPoint(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public PolyPoint() : this(0, 0)
+        {
+
+        }
+
+        public bool comparePoint(PolyPoint p)
+        {
+            if (Mathf.Abs(p.x - x) > .01f) return false;
+            if (Mathf.Abs(p.y - y) > .01f) return false;
+            return true;
+        }
+    }
+
+    private class PolyVector
+    {
+        public PolyPoint p1;
+        public PolyPoint p2;
+
+        public PolyVector(PolyPoint p1, PolyPoint p2)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+
+        public PolyVector() : this(new PolyPoint(), new PolyPoint())
+        {
+
+        }
+
+        public bool compareVector(PolyVector v)
+        {
+            if (v.p1.comparePoint(p1) && v.p2.comparePoint(p2)) return true;
+            if (v.p1.comparePoint(p2) && v.p2.comparePoint(p1)) return true;
+            return false;
+        }
+    }
+}
